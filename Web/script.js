91544decd2a0e7130c9653dd91aecbefab7d462c
Utsav mishra <125037233+UTSAV79@users.noreply.github.com>
2026-02-22 13:09:25 +0530

    Refactor webcam access and OpenCV initialization

--- Web/script.js
+++ Web/script.js
@@ -3,43 +3,48 @@ let canvas = document.getElementById('canvasOutput');
 let ctx = canvas.getContext('2d');
 
 // Access webcam
-navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-  video.srcObject = stream;
-});
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then((stream) => {
+    video.srcObject = stream;
+  })
+  .catch((err) => {
+    console.error("Camera access error:", err);
+  });
 
-// Wait until OpenCV.js is ready
-function onOpenCvReady() {
-  console.log('OpenCV.js is ready');
+// Wait until OpenCV is fully loaded
+cv['onRuntimeInitialized'] = () => {
+  console.log("OpenCV.js is ready");
 
   const cap = new cv.VideoCapture(video);
   const faceCascade = new cv.CascadeClassifier();
 
-  // Load the pre-trained face detection model (Haar Cascade)
-  let utils = new Utils('errorMessage');
-  utils.createFileFromUrl(
-    'haarcascade_frontalface_default.xml',
-    'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml',
-    () => {
+  // Load Haar cascade file
+  fetch('https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml')
+    .then(res => res.arrayBuffer())
+    .then(buffer => {
+      cv.FS_createDataFile('/', 'haarcascade_frontalface_default.xml', new Uint8Array(buffer), true, false, false);
       faceCascade.load('haarcascade_frontalface_default.xml');
       startDetection();
-    }
-  );
+    });
 
   function startDetection() {
     const frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     const gray = new cv.Mat();
+    const faces = new cv.RectVector();
 
     function processVideo() {
       cap.read(frame);
-      cv.cvtColor(frame, gray, cv.COLOR_RGBA2GRAY, 0);
 
-      let faces = new cv.RectVector();
-      faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0);
+      cv.cvtColor(frame, gray, cv.COLOR_RGBA2GRAY);
+      faceCascade.detectMultiScale(gray, faces, 1.1, 3);
 
+      // Draw video frame
       ctx.drawImage(video, 0, 0);
-      for (let i = 0; i < faces.size(); ++i) {
+
+      // Draw bounding boxes
+      for (let i = 0; i < faces.size(); i++) {
         let face = faces.get(i);
-        ctx.strokeStyle = '#00ff00';
+        ctx.strokeStyle = "#00ff00";
         ctx.lineWidth = 3;
         ctx.strokeRect(face.x, face.y, face.width, face.height);
       }
@@ -49,4 +54,4 @@ function onOpenCvReady() {
 
     processVideo();
   }
-}
+};
